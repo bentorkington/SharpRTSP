dbbe331316c2e6679d7e0218d05a6739df5e7750
Nicolas Graziano <nicolas.graziano@gmail.com>
2024-04-17 16:29:40 +0200

    Update version
    And add some tests for RawMediaFrame

---
+++ RTSP.Tests/Rtp/RawMediaFrameTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Rtsp.Rtp.Tests
+{
+    [TestFixture()]
+    public class RawMediaFrameTests
+    {
+        [Test()]
+        public void AnyTest()
+        {
+            List<ReadOnlyMemory<byte>> data = [
+                new byte[] {0x01 }.AsMemory(),
+              ];
+            RawMediaFrame rawMediaFrame = new(data, []) { ClockTimestamp = DateTime.MinValue, RtpTimestamp = 0 };
+            Assert.That(rawMediaFrame.Any(), Is.True);
+        }
+
+        [Test()]
+        public void AnyEmptyTest()
+        {
+            RawMediaFrame rawMediaFrame = RawMediaFrame.Empty;
+            Assert.That(rawMediaFrame.Any(), Is.False);
+        }
+
+
+        [Test()]
+        public void AnyDisposedTest()
+        {
+            {
+                // Validate that Empty RawMediaFrame do not throw ObjectDisposedException when used multiple times
+                using RawMediaFrame rawMediaFrame = RawMediaFrame.Empty;
+                Assert.That(rawMediaFrame.Any(), Is.False);
+            }
+            {
+                using RawMediaFrame rawMediaFrame = RawMediaFrame.Empty;
+                Assert.That(rawMediaFrame.Any(), Is.False);
+            }
+
+        }
+    }
+}
