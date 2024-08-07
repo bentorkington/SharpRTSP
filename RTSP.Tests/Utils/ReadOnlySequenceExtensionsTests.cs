309597ac8b81762cb2fa991f8a2561ab7bab3f31
Nicolas Graziano <nicolas.graziano@gmail.com>
2024-08-07 15:24:43 +0200

    Add search of end of line in ReadOnlySequence

---
+++ RTSP.Tests/Utils/ReadOnlySequenceExtensionsTests.cs
@@ -0,0 +1,172 @@
+﻿using NUnit.Framework;
+using System;
+using System.Buffers;
+using System.Linq;
+
+namespace Rtsp.Utils.Tests
+{
+    [TestFixture()]
+    public class ReadOnlySequenceExtensionsTests
+    {
+        public class TestSegment : ReadOnlySequenceSegment<byte>
+        {
+            public TestSegment(Memory<byte> memory)
+            {
+                Memory = memory;
+            }
+
+            public TestSegment Append(Memory<byte> memory)
+            {
+                var segment = new TestSegment(memory)
+                {
+                    RunningIndex = RunningIndex + Memory.Length
+                };
+                Next = segment;
+                return segment;
+            }
+        }
+
+        private static ReadOnlySequence<byte> CreateSequence(params byte[][] buffers)
+        {
+            var firstSegment = new TestSegment(buffers[0]);
+            var lastSegment = firstSegment;
+            for (int i = 1; i < buffers.Length; i++)
+            {
+                lastSegment = lastSegment.Append(buffers[i]);
+            }
+            return new ReadOnlySequence<byte>(firstSegment, 0, lastSegment, lastSegment.Memory.Length);
+        }
+
+
+        [Test()]
+        public void FindEndOfLineTestSimple()
+        {
+            var sequence = CreateSequence("\r\n"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Not.Null);
+            var (endOfLinePos, startOfLinePos) = result.Value;
+            Assert.That(sequence.Slice(sequence.Start, endOfLinePos).ToArray(), Is.Empty);
+            Assert.That(sequence.Slice(startOfLinePos).ToArray(), Is.Empty);
+        }
+
+        [Test()]
+        public void FindEndOfLineTestEmpty()
+        {
+            var sequence = CreateSequence(""u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Null);
+        }
+
+        [Test()]
+        public void FindEndOfLineNoCrLf()
+        {
+            var sequence = CreateSequence("azererze"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Null);
+        }
+
+        [Test()]
+        public void FindEndOfLineNoCrLfMulti()
+        {
+            var sequence = CreateSequence("azererze"u8.ToArray(), "fdsfsdf"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Null);
+        }
+
+        [Test()]
+        public void FindEndOfLineCrLfMulti()
+        {
+            var sequence = CreateSequence("azererze"u8.ToArray(), "fdsf\r\nsdf"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Not.Null);
+            var (endOfLinePos, startOfLinePos) = result.Value;
+            Assert.That(sequence.Slice(sequence.Start, endOfLinePos).ToArray(), Is.EqualTo("azererzefdsf"u8.ToArray()));
+            Assert.That(sequence.Slice(startOfLinePos).ToArray(), Is.EqualTo("sdf"u8.ToArray()));
+        }
+
+        [Test()]
+        public void FindEndOfLineCrMulti()
+        {
+            var sequence = CreateSequence("azererze"u8.ToArray(), "fdsf\rsd\nf"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Not.Null);
+            var (endOfLinePos, startOfLinePos) = result.Value;
+            Assert.That(sequence.Slice(sequence.Start, endOfLinePos).ToArray(), Is.EqualTo("azererzefdsf"u8.ToArray()));
+            Assert.That(sequence.Slice(startOfLinePos).ToArray(), Is.EqualTo("sd\nf"u8.ToArray()));
+        }
+
+        [Test()]
+        public void FindEndOfLineLfMulti()
+        {
+            var sequence = CreateSequence("azererze"u8.ToArray(), "fdsf\nsd\nf"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Not.Null);
+            var (endOfLinePos, startOfLinePos) = result.Value;
+            Assert.That(sequence.Slice(sequence.Start, endOfLinePos).ToArray(), Is.EqualTo("azererzefdsf"u8.ToArray()));
+            Assert.That(sequence.Slice(startOfLinePos).ToArray(), Is.EqualTo("sd\nf"u8.ToArray()));
+        }
+
+        [Test()]
+        public void FindEndOfLineCrLfMultiSplit()
+        {
+            var sequence = CreateSequence("azererze\r"u8.ToArray(), "\nsdf"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Not.Null);
+            var (endOfLinePos, startOfLinePos) = result.Value;
+            Assert.That(sequence.Slice(sequence.Start, endOfLinePos).ToArray(), Is.EqualTo("azererze"u8.ToArray()));
+            Assert.That(sequence.Slice(startOfLinePos).ToArray(), Is.EqualTo("sdf"u8.ToArray()));
+        }
+
+        [Test()]
+        public void FindEndOfLineCrMultiSplit()
+        {
+            var sequence = CreateSequence("azererze\r"u8.ToArray(), "sdf"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Not.Null);
+            var (endOfLinePos, startOfLinePos) = result.Value;
+            Assert.That(sequence.Slice(sequence.Start, endOfLinePos).ToArray(), Is.EqualTo("azererze"u8.ToArray()));
+            Assert.That(sequence.Slice(startOfLinePos).ToArray(), Is.EqualTo("sdf"u8.ToArray()));
+        }
+
+        [Test()]
+        public void FindEndOfLineLnMultiSplit()
+        {
+            var sequence = CreateSequence("azererze\n"u8.ToArray(), "sdf"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Not.Null);
+            var (endOfLinePos, startOfLinePos) = result.Value;
+            Assert.That(sequence.Slice(sequence.Start, endOfLinePos).ToArray(), Is.EqualTo("azererze"u8.ToArray()));
+            Assert.That(sequence.Slice(startOfLinePos).ToArray(), Is.EqualTo("sdf"u8.ToArray()));
+        }
+
+        [Test()]
+        public void FindEndOfLineFinalCrLnMultiSplit()
+        {
+            var sequence = CreateSequence("azererze"u8.ToArray(), "sdf\r\n"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Not.Null);
+            var (endOfLinePos, startOfLinePos) = result.Value;
+            Assert.That(sequence.Slice(sequence.Start, endOfLinePos).ToArray(), Is.EqualTo("azererzesdf"u8.ToArray()));
+            Assert.That(sequence.Slice(startOfLinePos).ToArray(), Is.Empty);
+        }
+
+        [Test()]
+        public void FindEndOfLineFinalLnMultiSplit()
+        {
+            var sequence = CreateSequence("azererze"u8.ToArray(), "sdf\n"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Not.Null);
+            var (endOfLinePos, startOfLinePos) = result.Value;
+            Assert.That(sequence.Slice(sequence.Start, endOfLinePos).ToArray(), Is.EqualTo("azererzesdf"u8.ToArray()));
+            Assert.That(sequence.Slice(startOfLinePos).ToArray(), Is.Empty);
+        }
+
+        [Test()]
+        public void FindEndOfLineFinalCrMultiSplit()
+        {
+            var sequence = CreateSequence("azererze"u8.ToArray(), "sdf\r"u8.ToArray());
+            var result = sequence.FindEndOfLine();
+            Assert.That(result, Is.Null);
+        }
+    }
+}
---
+++ RTSP/Utils/ReadOnlySequenceExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+
+namespace Rtsp.Utils
+{
+    public static class ReadOnlySequenceExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static (SequencePosition endOfLinePos, SequencePosition startOfLinePos)? FindEndOfLine(in this ReadOnlySequence<byte> buffer)
+        {
+            const byte lf = (byte)'\n';
+            var crlf = "\r\n"u8;
+
+            var startOfSegment = buffer.Start;
+            var position = startOfSegment;
+            var findPos = buffer.Start;
+            var checkForEndOfLine = false;
+            while (buffer.TryGet(ref position, out var segment))
+            {
+                if (segment.Length == 0)
+                    continue;
+
+                if (checkForEndOfLine)
+                {
+                    var nbToskip = segment.Span[0] == lf ? 1 : 0;
+                    return (findPos, buffer.GetPosition(nbToskip, startOfSegment));
+                }
+
+                var pos = segment.Span.IndexOfAny(crlf);
+                if (pos != -1)
+                {
+                    findPos = buffer.GetPosition(pos, startOfSegment);
+                    // handle \n only
+                    if (segment.Span[pos] == lf)
+                    {
+                        return (findPos, buffer.GetPosition(pos + 1, startOfSegment));
+                    }
+                    if (pos + 1 < segment.Length)
+                    {
+                        // handle \r\n ou \r other in same segment
+                        var nbToskip = segment.Span[pos + 1] == lf ? 2 : 1;
+                        return (findPos, buffer.GetPosition(pos + nbToskip, startOfSegment));
+                    }
+
+                    // need to wait for next segment
+                    checkForEndOfLine = true;
+                }
+
+                startOfSegment = position;
+            }
+            return default;
+        }
+    }
+}
