ebc94214a0a4b42447ccb47ea2842adc15ece9ac
Nicolas Graziano <nicolas.graziano@gmail.com>
2024-10-27 10:50:52 +0100

    Some style

--- RTSP/Messages/RTSPHeaderNames.cs
+++ RTSP/Messages/RTSPHeaderNames.cs
@@ -1,26 +1,25 @@
-﻿namespace Rtsp.Messages
+﻿namespace Rtsp.Messages;
+
+/// <summary>
+/// Class containing helper constant for general use headers.
+/// </summary>
+public static class RtspHeaderNames
 {
-    /// <summary>
-    /// Class containing helper constant for general use headers.
-    /// </summary>
-    public static class RtspHeaderNames
-    {
-        public const string ContentBase = "Content-Base";
-        public const string ContentEncoding = "Content-Encoding";
-        public const string ContentType = "Content-Type";
+    public const string ContentBase = "Content-Base";
+    public const string ContentEncoding = "Content-Encoding";
+    public const string ContentType = "Content-Type";
 
-        public const string Public = "Public";
-        public const string Session = "Session";
-        public const string Transport = "Transport";
-        public const string CSeq = "CSeq";
+    public const string Public = "Public";
+    public const string Session = "Session";
+    public const string Transport = "Transport";
+    public const string CSeq = "CSeq";
 
-        public const string WWWAuthenticate = "WWW-Authenticate";
-        public const string Authorization = "Authorization";
+    public const string WWWAuthenticate = "WWW-Authenticate";
+    public const string Authorization = "Authorization";
 
-        public const string RateControl = "Rate-Control";
-        public const string Require = "Require";
+    public const string RateControl = "Rate-Control";
+    public const string Require = "Require";
 
-        public const string Range = "range";
-        public const string Scale = "scale";
-    }
+    public const string Range = "range";
+    public const string Scale = "scale";
 }
--- RTSP/Messages/RTSPHeaderUtils.cs
+++ RTSP/Messages/RTSPHeaderUtils.cs
@@ -1,20 +1,13 @@
+namespace Rtsp.Messages;
+
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Rtsp.Messages
+public static class RTSPHeaderUtils
 {
-    public static class RTSPHeaderUtils
-    {
-        public static IList<string> ParsePublicHeader(string? headerValue)
-        {
-            if (string.IsNullOrEmpty(headerValue))
-            {
-                return [];
-            }
-            return headerValue!.Split(',').Select(m => m.Trim()).ToList();
-        }
+    public static IList<string> ParsePublicHeader(string? headerValue) => 
+        string.IsNullOrEmpty(headerValue) ? [] : headerValue!.Split(',').Select(m => m.Trim()).ToList();
 
-        public static IList<string> ParsePublicHeader(RtspResponse response)
-            => ParsePublicHeader(response.Headers.TryGetValue(RtspHeaderNames.Public, out var value) ? value : null);
-    }
+    public static IList<string> ParsePublicHeader(RtspResponse response)
+        => ParsePublicHeader(response.Headers.TryGetValue(RtspHeaderNames.Public, out var value) ? value : null);
 }
